eea3ec28742ee71ce26f7848d22e2a7ac47c039b
csm-kr <csm8167@naver.com>
2020-04-01 07:42:07 +0900

    scaling gray images and ab images

--- dataset.py
+++ dataset.py
@@ -7,10 +7,10 @@ from skimage import color
 import numpy as np
 import torch
 import cv2
-import torchvision
+import torchvision.transforms as transforms
 
 
-visualization = False
+visualization = True
 
 
 class ColorDataset(Dataset):
@@ -41,30 +41,41 @@ class ColorDataset(Dataset):
             image = self.transform(image)
 
         # rgb to lab
-        # L lies between 0 and 100, and a and b lie between -110 and 110.
+        # L lies between 0 and 100, and a and b lie between -128 to 127.
+        image = np.asarray(image)  # uint 8 0 ~ 255
+
+        img_gray = color.rgb2gray(image)  # 0 ~ 1 scale float64
+        img_gray = img_gray[:, :, np.newaxis]  # h, w, 1
 
         img_lab = color.rgb2lab(image)   # type --> uint8 to float64
-        img_l = img_lab[:, :, 0]         # h, w
-        img_l = img_l[:, :, np.newaxis]  # h, w, 1
+        img_lab = (img_lab + 128) / 255  # 0 ~ 1 scaling float 64
+
         img_ab = img_lab[:, :, 1:]       # h, w, 2
 
         # visualization
         if visualization:
 
             # lab to rgb
-            color_img = np.concatenate((img_l, img_ab), axis=-1)
+            img_gray_vis = img_gray * 100
+            img_ab_vis = img_ab * 255 - 128
+
+            color_img = np.concatenate((img_gray_vis, img_ab_vis), axis=-1)
             color_img = color.lab2rgb(color_img)  # rgb
+
             cv2.imshow('color', color_img[..., ::-1])
+            cv2.imshow('gray', img_gray)
             cv2.waitKey(0)
 
-        img_l = img_l.transpose((2, 0, 1))    # 1, h, w
+        img_gray = img_gray.transpose((2, 0, 1))    # 1, h, w
         img_ab = img_ab.transpose((2, 0, 1))  # 2, h, w
 
-        # .type(torch.FloatTensor) 을 해주지 않으면 torch.DoubleTensor 라서 오류가 납니다. :)
-        img_l = torch.from_numpy(img_l).type(torch.FloatTensor)
-        img_ab = torch.from_numpy(img_ab).type(torch.FloatTensor)
+        # .type(torch.FloatTensor) 을 해주지 않으면 torch.DoubleTensor 라서 오류가 납니다. :) <-- 어디서?
+        # img_gray = torch.from_numpy(img_gray).type(torch.FloatTensor)
+        # img_ab = torch.from_numpy(img_ab).type(torch.FloatTensor)
+        img_gray = torch.from_numpy(img_gray)
+        img_ab = torch.from_numpy(img_ab)
 
-        return img_l, img_ab
+        return img_gray, img_ab
 
     def __len__(self):
         return len(self.image_name)
@@ -73,11 +84,12 @@ class ColorDataset(Dataset):
 # test code
 if __name__ == "__main__":
 
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((256, 256))
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        # transforms.RandomCrop(224),
     ])
 
     train_dataset = ColorDataset(root='D:\Data\VOC_ROOT\TEST\VOC2007\JPEGImages', subset='test', transform=transform)
 
-    for i in train_dataset:
+    for i, (g, ab) in enumerate(train_dataset):
         print(i)
